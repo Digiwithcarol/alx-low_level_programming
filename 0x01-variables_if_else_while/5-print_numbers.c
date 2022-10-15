0bbb486f090c77893dfd53398b38cfbde7adef83
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-15 04:49:23 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - prints all single digit number 10 starting from 0 with a new line
+*Return: 0
+*/
+
+int main(void)
+{
+
+	int ba;
+
+	for (ba = 0; ba < 10; ba++)
+
+	printf("%d", ba);
+
+	printf("\n");
+
+	return (0);
+}
