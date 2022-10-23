e68d9f583b14cd92ad65d66cd15f75bc677d6fb6
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-23 16:25:40 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 
 /**
 * main - Entry lowercase alphabet
+*print_ alphabet is good
 * Return: 0
 */
 
 void print_alphabet(void)
 {
 	char alp;
-	
+
 	for (alp = 'a'; alp <= 'z'; alp++)
 {
 	_putchar(alp);
