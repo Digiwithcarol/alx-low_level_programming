ead37f7424c82265c53046261d16faafd38b7b2e
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-25 08:47:33 -0700

    TAsk 0 trial

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * reset_to_98 - a function that takes a pointer to an integer
 * @n: integer to be defined
-* Return: n
+* Return: void
 */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* swap_int - swap values of two nums
+* @: first integer
+* @: second pointer
+* Return: void or nothing
+*/
+
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a;
+	*a = *b;
+	*b = constant;
+}
+
