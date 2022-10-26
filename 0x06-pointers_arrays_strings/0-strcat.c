a670b272b3fbb3ecd393078078b849c364b1bc15
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-26 13:10:02 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,21 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int count = 0, count2 = 0;
+	int i;
+	int j;
 
-	while (*(dest + count) != '\0')
-{
-	count++;
-}
-	while (count2 >= 0)
-{
-	*(dest + count) = *(src + count2);
-	if (*(src + count2) == '\0')
-		break;
-		count++;
-		count2++;
-
+	i = 0;
+	while (desk[i] != '\0')
+	{
+		i++;
 	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
