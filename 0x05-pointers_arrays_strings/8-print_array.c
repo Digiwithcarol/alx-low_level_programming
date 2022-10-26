12f3cd3bbd36a45a228296195200006f98cf6082
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-26 10:33:43 -0700

    task 08

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (i < n; i++)
+	for (i = 0; i < n; i++)
 {
-	printf("%d", *(a + i);
+	printf("%d", a[i]);
 	if (i != (n - 1))
-		print(";");
+		printf(",")
 }
-	print("\n");
+	printf("\n");
 }
