dc423bd859b63d13ca44fdd65117dd52c50bbf49
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-26 09:03:18 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,17 +11,13 @@ Void puts2(char *str)
 
 	int count = 0;
 
-	while (count >= 0)
-	{
-	if (str[count] == '\0')
-	}
+	for (; str[count] != '\0'; count++)
 {
+	if ((count % 2) == '\0')
+	_putchar(str[count]);
+
+	else
+		continue;
+}
 	_putchar('\n');
-	break;
-	}
-{
-	if (count % 2 == 0)
-		_putchar(str[count];
-	count++;
-	}
 }
