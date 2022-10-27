3afdf822f9a84ff4d5faea4558b42cd0a8082c52
Digiwithcarol <carolynokenwa@yahoo.com>
2022-10-27 12:53:10 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,29 @@
 
 int main(void)
 {
-	int i, k ='0', j;
-	for(i = '0'; i <= '9'; i++)
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
 	{
 	for (j = k; j <= 9; j++)
 	{
-	if(i! = j)
+	if (i != j)
 	{
 	putchar(i);
 	putchar(j);
 	}
-	if(i! = j)
+	if (i != j)
 	{
 	continue;
 	}
-	if(i == 8 && j == 9)
+	if (i == 8 && j == 9)
 	{
 	break;
 	}
 	else
 	{
 	putchar(',');
-	putchar('');
+	putchar(' ');
 	}
 	}
 	k++;
